70dd333990511ee6040ab7f0002845282ad2a2e3
amrou <163398359+amrouj37@users.noreply.github.com>
2026-04-17 10:28:09 +0100

    Delete OneDrive/Desktop/projet_adam_final directory

--- OneDrive/Desktop/projet_adam_final/Controller/supprimer_fournisseur.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-include_once '../controller/fournisseur.php';
-$fournisseur=new fournisseurC();
-$fournisseur->supprimer($_GET["id_fournisseur"]);
-header('location:../View/index.php');
-?>
